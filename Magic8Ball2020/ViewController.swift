904301fcb027301e1d4bc148a6ab03dea0df88a9
cwalter50 <walt50@gmail.com>
2020-09-17 09:55:41 -0400

    Initial Commit

---
+++ Magic8Ball2020/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Magic8Ball2020
+//
+//  Created by  on 9/17/20.
+//  Copyright © 2020 DocsApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
