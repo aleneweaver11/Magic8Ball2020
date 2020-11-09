b70ba0a9dd43c7132c8d086c2c9132f4d84f82c1
aleneweaver11 <aleneweaver11@gmail.com>
2020-11-08 22:19:10 -0500

    magic 8 ball

--- Magic8Ball2020/ViewController.swift
+++ Magic8Ball2020/ViewController.swift
@@ -8,13 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var myTextField: UITextField!
+    @IBOutlet weak var myLabel: UILabel!
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func revealFortune()
+    {
+         let randomInt = Int.random(in: 1...5)
+            var response = ""
+            if randomInt == 1
+            {
+               response = "Yes"
+            }
+            else if randomInt == 2
+            {
+                response = "No"
+            }
+            else if randomInt == 3
+            {
+                response = "Maybe"
+            }
+            else if randomInt == 4
+            {
+                response = "ask again later"
+            }
+            else
+            {
+                response = "Don't count on it"
+            }
+                
+            let question = myTextField.text
+            myLabel.text = question
+            myLabel.text = question! + "\n" + response
+            myTextField.text = ""
+        }
+    
+    @IBAction func buttonTapped(_ sender: UIButton)
+    {
+        revealFortune()
+    }
+    //override func motionEnded(_ motion: UIEvent.EventSubtype, with )
 }
 
